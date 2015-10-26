0d4f30a7e5ff28daf9ee8d609fdeef28bc80ae26
nndio <anastasia.diordiev@gmail.com>
2015-10-26 18:38:08 +0200

    Add owner param. Styles for received and sent messages.

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import Avatar from './Avatar';
 import styles from './Chat.css';
 
+var owner = 2;
+
 function getTimeStamp(msgTime) {
     var date = new Date(msgTime*1000);
     var hours = date.getHours();
@@ -12,10 +14,11 @@ function getTimeStamp(msgTime) {
 class MsgBox extends Component {
     render() {
         return(
-            <div key={this.props.msg} className={styles.u_msg}>
+            <div key={this.props.msg} className={(owner === this.props.sender) ? styles.my_msg : styles.u_msg}>
                 <div className={styles.content_msg}> {this.props.msg} </div>
                 <div className={styles.footer_msg}>
                     {
+                        !(owner === this.props.sender) &&
                         <div>
                             <div style={{float: 'left'}}> {this.props.name} </div>
                         </div>
@@ -45,16 +48,19 @@ export default class Chat extends Component {
         <div className={styles.base}>{
           this.props.messages.map( message => {
             return <div key={message.id} className={styles.msgBox}>
-              <Avatar
-                      src={message.avatar}
-                      name={message.name}
-                  />
-                <div className={styles.arrowLeft}></div>
+                {
+                    !(owner === message.sender) &&
+                    <Avatar style={styles.avatar}
+                            src={message.avatar}
+                            name={message.name}
+                        />
+                }
+                <div className={(owner === message.sender) ? styles.arrowRight : styles.arrowLeft}></div>
                 <MsgBox msg={ message.msg} name={message.name} time={message.time} sender={message.sender} />
             </div>
           })
         }
-        </div>
+        </div> 
     );
   }
 
